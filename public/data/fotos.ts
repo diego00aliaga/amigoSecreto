7f7d63dfd9f19c31318b54a52f838deefffbb201
Diego Aliaga <diegoaliaga@Diegos-MacBook-Air.local>
2025-12-03 16:50:44 -0300

    feat

---
+++ public/data/fotos.ts
@@ -0,0 +1,17 @@
+export const LISTA_DE_FOTOS = [
+    "tio_ramon.png",
+    "arbol.png",
+    "gorrito.png",
+    "campanitas.png",
+    "bernardita.png",
+    "gaby.png",
+    "mama.png",
+    "tiaMari.png",
+    "flo.png",
+    "copiapo.png",
+    "diego.png",
+    "isa.png",
+    "seba.png",
+    "yaya.png"
+    // ... resto de fotos
+  ];
